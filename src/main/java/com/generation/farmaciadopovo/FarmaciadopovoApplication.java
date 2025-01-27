32248939b16b457852a43190ab3e58e23fa5572c
Henrique <119366049+HenriqueAndras1@users.noreply.github.com>
2025-01-27 09:45:18 -0300

    Estou enviando as configurações do meu projeto dentro da, pom.xml e do arquivo application.properties, assim criando meu banco de dados db_farmacia do povo.

---
+++ src/main/java/com/generation/farmaciadopovo/FarmaciadopovoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciadopovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciadopovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciadopovoApplication.class, args);
+	}
+
+}
