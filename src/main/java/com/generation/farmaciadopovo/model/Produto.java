9b44db1a33eb0394bcd54a90bf1dbcc05380b3d7
Henrique <119366049+HenriqueAndras1@users.noreply.github.com>
2025-01-27 15:21:15 -0300

    Foi criada a classe de usuario, para poder fazer o cadastro do mesmo.

--- src/main/java/com/generation/farmaciadopovo/model/Produto.java
+++ src/main/java/com/generation/farmaciadopovo/model/Produto.java
@@ -33,6 +33,11 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+	
 
 	public Long getId() {
 		return id;
@@ -73,6 +78,12 @@ public class Produto {
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
-	
-	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
